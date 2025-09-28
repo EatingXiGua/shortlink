af37278b272437eca5386fe650a24336f461c363
xvke1 <xvke0127@163.com>
2025-09-28 19:41:42 +0800

    feature: 开发短链接项目的基础代码

---
+++ project/src/main/java/com/kkk/shortlink/project/ShortLinkApplication.java
@@ -0,0 +1,13 @@
+package com.kkk.shortlink.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.kkk.shortlink.project.dao.mapper")
+public class ShortLinkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkApplication.class, args);
+    }
+}
