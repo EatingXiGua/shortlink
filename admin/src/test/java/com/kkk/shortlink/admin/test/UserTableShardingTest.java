07b24153295886401ee0f2a5dbf1064a01f5ba2d
xvke1 <xvke0127@163.com>
2025-09-30 18:58:21 +0800

    feature: test文件，生成sql语句的作用

--- admin/src/test/java/com/kkk/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/kkk/shortlink/admin/test/UserTableShardingTest.java
@@ -2,27 +2,30 @@ package com.kkk.shortlink.admin.test;
 
 public class UserTableShardingTest {
 
-    public static final String SQL = "CREATE TABLE `t_user_%d` (\n" +
+    public static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
             "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `username` varchar(256) DEFAULT NULL COMMENT '用户名',\n" +
-            "  `password` varchar(512) DEFAULT NULL COMMENT '密码',\n" +
-            "  `real_name` varchar(256) DEFAULT NULL COMMENT '真实姓名',\n" +
-            "  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',\n" +
-            "  `mail` varchar(512) DEFAULT NULL COMMENT '邮箱',\n" +
-            "  `deletion_time` bigint DEFAULT NULL COMMENT '注销时间戳',\n" +
+            "  `domain` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '域名',\n" +
+            "  `short_uri` varchar(8) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL COMMENT '短链接',\n" +
+            "  `full_short_url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '完善短链接',\n" +
+            "  `origin_url` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '原始链接',\n" +
+            "  `click_num` int(11) unsigned zerofill DEFAULT NULL COMMENT '点击量',\n" +
+            "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '分组标识',\n" +
+            "  `favicon` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '网站图标',\n" +
+            "  `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0启用 1未启用',\n" +
+            "  `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0接口创建 1控制台创建',\n" +
+            "  `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0永久有效 1自定义有效期',\n" +
+            "  `valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
+            "  `describe` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '描述',\n" +
             "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
             "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
-            "  PRIMARY KEY (`id`)\n" +
-            ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+            "  PRIMARY KEY (`id`),\n" +
+            "  KEY `ide_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1972198121188306947 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
 
     public static void main(String[] args) {
-//        for (int i = 0; i < 16; i++) {
-//            System.out.printf((SQL)+"%n",i);
-//        }
-
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("abc_%d", i);
+        for (int i = 0; i < 16; i++) {
+            System.out.printf((SQL)+"%n",i);
         }
     }
 }
